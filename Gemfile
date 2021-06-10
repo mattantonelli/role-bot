6cf96036651b2ce1f8e696796dc0fede6bed2438
Matt Antonelli <contact@raelys.com>
2021-06-10 00:18:25 +0000

    Add Capistrano for deployments

--- Gemfile
+++ Gemfile
@@ -2,3 +2,9 @@ source 'https://rubygems.org'
 ruby '>= 2.7.2'
 
 gem 'discordrb', git: 'https://github.com/mattantonelli/discordrb', branch: 'button-interactions'
+
+group :development do
+  gem 'capistrano', '3.10.2'
+  gem 'capistrano-bundler', '~> 1.2'
+  gem 'capistrano-rbenv', '~> 2.1'
+end
---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+# config valid only for current version of Capistrano
+lock '3.10.2'
+
+set :application, 'role-bot'
+set :repo_url,    'https://github.com/mattantonelli/role-bot'
+set :branch,      ENV['BRANCH_NAME'] || 'master'
+set :deploy_to,   '/var/bot/role'
+set :default_env, { path: '$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH' }
+
+# rbenv
+set :rbenv_type, :user
+set :rbenv_ruby, '2.7.2'
+
+namespace :deploy do
+  desc 'Symlink configuration'
+  after :updating, :symlink_config do
+    on roles(:app) do
+      execute :ln, '-s', shared_path.join('config.yml'), release_path.join('config/config.yml')
+    end
+  end
+
+  desc 'Restart the bot'
+  after :publishing, :restart do
+    on roles(:app) do
+      invoke 'deploy:stop'
+      invoke 'deploy:start'
+    end
+  end
+
+  desc 'Start the bot'
+  task :start do
+    on roles(:app) do
+      within release_path do
+        info 'Starting the bot'
+        execute(*%w(screen -d -S rolebot -m bundle exec ruby run.rb))
+      end
+    end
+  end
+
+  desc 'Stop the bot'
+  task :stop do
+    on roles(:app) do
+      info 'Stopping the bot'
+      execute 'trap "screen -S rolebot -X quit" QUIT TERM INT EXIT'
+    end
+  end
+end
